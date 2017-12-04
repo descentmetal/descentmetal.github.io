3c259f8045a1659ffba451a84dd1e4db345415d4
adnroy <adnroy@github.com>
2017-12-04 01:33:18 -0700

    initialize

---
+++ data/CedarMesa_5.js
@@ -0,0 +1,7 @@
+var json_CedarMesa_5 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Cedar Mesa", "Image": "\/Users\/ARoy\/Desktop\/new pix\/31136256553_319850de9e_k (2).jpg", "Desc": "With around 56,000 sites featuring everything from cliff dwellings to arrowheads, Cedar Mesa is one of the richest archeological landscapes in the region. (Photo: BLM\/Bob Wick)" }, "geometry": { "type": "Point", "coordinates": [ -109.913436660047921, 37.439744902723255 ] } }
+]
+}
---
+++ data/GrandGulch_4.js
@@ -0,0 +1,7 @@
+var json_GrandGulch_4 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Grand Gulch", "Image": "\/Users\/ARoy\/Desktop\/new pix\/31136257533_bc092e7138_k (1) (1).jpg", "Desc": "Backpackers who strike out into the backcountry of Grand Gulch are literally walking through history: rock art and Ancestral Puebloan ruins line the walls of this 52-mile-long canyon (Photo: BLM\/Bob Wick)" }, "geometry": { "type": "Point", "coordinates": [ -110.22169840493784, 37.346285943611342 ] } }
+]
+}
